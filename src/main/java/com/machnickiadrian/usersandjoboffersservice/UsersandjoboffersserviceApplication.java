83b457c7e75bb55ed7902f39f13e3532e499078a
Adrian Machnicki <machnickiadrian@gmail.com>
2018-12-26 12:30:36 +0100

    Initial commit

---
+++ src/main/java/com/machnickiadrian/usersandjoboffersservice/UsersandjoboffersserviceApplication.java
@@ -0,0 +1,14 @@
+package com.machnickiadrian.usersandjoboffersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersandjoboffersserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersandjoboffersserviceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/machnickiadrian/usersandjoboffersservice/UsersandjoboffersserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.machnickiadrian.usersandjoboffersservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsersandjoboffersserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
